c6b81041455c457fb52c7bd76ca7d77c2103c013
sohail-akhtar-git <mcgsameeryt786@gmail.com>
2025-01-17 14:04:31 +0530

    changes

--- src/components/SearchPage/Search.js
+++ src/components/SearchPage/Search.js
@@ -17,7 +17,7 @@ function Search() {
   useEffect(() => {
     var query = convertToSolrQuery(id, "description", "");
 
-    let url = `http://localhost:8983/solr/productCore/select?indent=true&q.op=OR&q=${query}`; // Use the id to fetch product data from the API
+    let url = `/api/search?query=${query}`; // Use the id to fetch product data from the API
     console.log(url);
     // console.log(query);
     fetch(url)
